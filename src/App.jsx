6b2b6911b6fe301d1f0b174c2d60ec7c10fc2ae1
鈴木竜太 <django.contrib.auth.models@gmail.com>
2025-05-18 17:11:45 +0900

    install textalive and setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Player } from "textalive-app-api";
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 単語が発声されていたら #text に表示する
+  const animateWord = function (now, unit) {
+    if (unit.contains(now)) {
+      document.querySelector("#text").textContent = unit.text;
+    }
+  };
+
+  // TextAlive Player を作る
+  const apiToken = import.meta.env.VITE_TEXTALIVE_API_TOKEN;
+  console.log(apiToken);
+  const player = new Player({ app: { token: apiToken } });
+  player.addListener({
+    // 動画オブジェクトの準備が整ったとき（楽曲に関する情報を読み込み終わったとき）に呼ばれる
+    onVideoReady: (v) => {
+      // 定期的に呼ばれる各単語の "animate" 関数をセットする
+      let w = player.video.firstWord;
+      while (w) {
+        w.animate = animateWord;
+        w = w.next;
+      }
+    },
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <span id="text">TextAlive references here.</span>
     </>
   )
 }
