5a3fd2c75184770cc1effeebaf40722cde619c9b
鈴木竜太 <django.contrib.auth.models@gmail.com>
2025-05-24 16:46:44 +0900

    outer definition onVideoReady function

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,40 @@ function App() {
   // TextAlive Player を作る
   const apiToken = import.meta.env.VITE_TEXTALIVE_API_TOKEN;
   console.log(apiToken);
-  const player = new Player({ app: { token: apiToken } });
-  player.addListener({
-    // 動画オブジェクトの準備が整ったとき（楽曲に関する情報を読み込み終わったとき）に呼ばれる
-    onVideoReady: (v) => {
-      // 定期的に呼ばれる各単語の "animate" 関数をセットする
-      let w = player.video.firstWord;
-      while (w) {
-        w.animate = animateWord;
-        w = w.next;
-      }
-    },
+  const player = new Player({
+    app: { token: apiToken },
+    mediaElement: document.querySelector('#media')
   });
+
+  // Register event listeners
+  player.addListener({
+    // onAppReady,
+    onVideoReady,
+    // onTimerReady,
+    // onThrottledTimeUpdate,
+    // onPlay,
+    // onPause,
+    // onStop,
+  });
+
+  const artistSpan = document.querySelector('#artist span')
+  const songSpan = document.querySelector('#song span')
+
+  // 動画オブジェクトの準備が整ったとき（楽曲に関する情報を読み込み終わったとき）に呼ばれる
+  // param v: IVideo
+  // https://developer.textalive.jp/packages/textalive-app-api/interfaces/ivideo.html
+  function onVideoReady(v) {
+    // metadata
+    artistSpan.textContent = player.data.song.artist.name;
+    songSpan.textContent = player.data.song.name;
+
+    // 定期的に呼ばれる各単語の "animate" 関数をセットする
+    let w = player.video.firstWord;
+    while (w) {
+      w.animate = animateWord;
+      w = w.next;
+    }
+  }
 
   return (
     <>
