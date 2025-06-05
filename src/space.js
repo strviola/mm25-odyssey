e16add819bdef9f17c57af73e87936ed6b53f8e6
baba <baba@pixiv.co.jp>
2025-06-05 12:59:57 +0900

    add one asteroid object

--- src/space.js
+++ src/space.js
@@ -1,4 +1,4 @@
-import { Simulation, SpaceObjectPresets } from 'spacekit.js';
+import { Simulation, SpaceObjectPresets, Ephem } from 'spacekit.js';
 
 const viz = new Simulation(document.getElementById('spacekit-container'), {
     basePath: './node_modules/spacekit.js/src',
@@ -19,3 +19,22 @@ viz.createObject('jupiter', SpaceObjectPresets.JUPITER);
 viz.createObject('saturn', SpaceObjectPresets.SATURN);
 viz.createObject('uranus', SpaceObjectPresets.URANUS);
 viz.createObject('neptune', SpaceObjectPresets.NEPTUNE);
+
+// generate asteroid
+const asteroid = new Ephem({
+    a: 7.55,
+    e: 0.8726,
+    i: 110.55 * Math.PI / 180,
+    om: 134.9919 * Math.PI / 180,
+    w: 152.76 * Math.PI / 180,
+    ma: 0,
+    epoch: Math.random() * 2500000,
+});
+
+viz.createObject('asteroid_1', {
+    hideOrbit: false,
+    particleSize: 10,
+    labelText: 'Asteroid',
+    textureUrl: '{{assets}}/sprites/fuzzyparticle.png',
+    ephem: asteroid,
+});
